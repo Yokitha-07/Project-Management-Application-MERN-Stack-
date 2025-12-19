5c4e49354fa147f788e96d449ef094793a6c7f08
Yokitha R <rathinarasayokitha1953@gmail.com>
2025-12-19 22:03:47 +0530

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require("mongoose");
+const app = express();
+const taskRoutes = require('./routers/taskRoute');
+const cors = require('cors');
+
+//Middleware
+app.use((req,res,next)=>{
+    console.log("path "+ req.path + " method "+ req.method);
+    next();
+})
+
+app.use(express.json());
+
+app.use(cors())
+
+/* app.get('/',(req,res)=>{          //api
+    res.send("Hello World");
+}); */
+
+//DB connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> {
+        app.listen(process.env.PORT, () =>{
+            console.log("DB connected successfully and listening to "+process.env.PORT);
+        });
+    })
+    .catch((error) => console.log(error));
+
+    app.use('/api/tasks', taskRoutes)
+
+
